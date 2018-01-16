1dccf3be41b9bb1bb7fa635a20c34c63cad16b55
Alexnymous <32875309+Alexnymous@users.noreply.github.com>
2018-01-16 10:19:48 +0100

    correction

--- include/login.inc.php
+++ include/login.inc.php
@@ -1,16 +1,43 @@
 <h1>Login</h1>
-
-<form action="#" method="post">
-    <div>
-        <label for="mail">adresse mail</label>
-        <input type="text" name="mail">
-    </div>
-    <div>
-        <label for="password">Mot de Passe</label>
-        <input type="password" name="password">
-    </div>
-    <div>
-       <input type="reset" value="effacer">
-        <input type="submit" name="login" value="envoyer">
-    </div>
-</form>
+<?php
+if (isset($_POST['login'])) {
+    $tabErreur = array();
+    $mail = $_POST['mail'];
+    $password = $_POST['password'];
+    if ($mail == "")
+        array_push($tabErreur, "Veuillez saisir une adresse");
+    if ($password == "")
+        array_push($tabErreur, "Veuillez saisir un mot de passe");
+    if (count($tabErreur) > 0) {
+        $message = "<ul>";
+        for ($i = 0 ; $i < count($tabErreur) ; $i++) {
+            $message .= "<li>" . $tabErreur[$i] . "</li>";
+        }
+        $message .= "</ul>";
+        echo ($message);
+        include ("./include/formLogin.php");
+    }
+    else {
+        $connexion = mysqli_connect("localhost", "NFactoryBlog", "NFactoryBlog", "nfactoryblog");
+        if (!$connexion) {
+            die("Erreur MySQL " . mysqli_connect_errno() . " : " . mysqli_connect_error());
+        }
+        else {
+            $password = sha1($password);
+            $requete = "SELECT * FROM t_users WHERE USERMAIL='$mail' AND USERPASSWORD='$password'";
+            if($result = mysqli_query($connexion, $requete)) {
+                if (mysqli_num_rows($result) > 0) {
+                    $_SESSION['login'] = 1;
+                    echo ("<script>redirection(\"index.php?page=accueil\");</script>
+                    <a href=\"index.php?page=accueil\">Vous êtes authentifié, viendez à la page d'accueil</a>");
+                }
+                else
+                    $_SESSION['login'] = 0;
+            }
+        }
+        mysqli_close($connexion);
+    }
+}
+else {
+    include ("./include/formLogin.php");
+}
