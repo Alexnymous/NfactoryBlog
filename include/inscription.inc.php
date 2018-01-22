3ce14308c3e18a3db693ebc24167589ade4a6e34
Alexnymous <32875309+Alexnymous@users.noreply.github.com>
2018-01-22 12:18:50 +0100

    création PDO inscription

--- include/inscription.inc.php
+++ include/inscription.inc.php
@@ -1,59 +1,26 @@
 <h1>Inscription</h1>
 <?php
-if(isset($_POST["formulaire"])) {
-    $tabErreur = array();
-    $nom = $_POST['nom'];
-    $prenom = $_POST['prenom'];
-    $mail = $_POST['mail'];
-    $mdp = $_POST['mdp'];
-    $confirmdp = $_POST['confirmdp'];
-    if($_POST["nom"] == "")
-        array_push($tabErreur, "Veuillez saisir votre nom");
-    if(filter_var($nom, FILTER_SANITIZE_STRING))
-    if($_POST["prenom"] == "")
-        array_push($tabErreur, "Veuillez saisir votre prénom");
-    if(!filter_var($prenom, FILTER_SANITIZE_STRING));
+$dsn = "mysql:dbname=nfactoryblog;
+        host=localhost;
+        charset=utf8";
 
-    if (!filter_var($mail, FILTER_VALIDATE_EMAIL)){
-        ;
-    }
-    $hash = password_hash($mdp, PASSWORD_DEFAULT);
-    $confirm = password_verify($confirmdp, $hash);
+$username = "root";
+$password = "";
 
-    if($confirm){
-        ;
-    } else{
-        array_push($tabErreur, "Veuillez saisir votre mot de passe");
+if(isset($_POST["nom"]) && ($_POST["prenom"]) && ($_POST["mail"]) && ($_POST["mdp"]) && ($_POST["mdp"]) && ($_POST["confirmdp"])) {
+
+    if($_POST ["mdp"] == $_POST["confirmdp"]){
+        $hash = password_hash($mdp, PASSWORD_DEFAULT);
+        $query = $pdo -> prepare ("INSERT INTO t_users (ID_USER, USERNAME, USERFNAME,USERMAIL, USERPASSWORD, USERDATEINS, T_ROLES_ID_ROLE) VALUES (NULL, '$nom', '$prenom', '$mail', '$hash', NULL, 5)");
+        $query->bindParam(':USERNAME', $_POST["nom"]);
+        $query->bindParam(':USERFNAME', $_POST["prenom"]);
+        $query->bindParam(':USERMAIL', $_POST["mail"]);
+        $query->bindParam(':USERPASSWORD', $hash);
+        $query->execute();
+        exit();
     }
 
-    if(count($tabErreur) != 0) {
-        $message = "<ul>";
-        for($i = 0 ; $i < count($tabErreur) ; $i++) {
-            $message .= "<li>" . $tabErreur[$i] . "</li>";
-        }
-        $message .= "</ul>";
-        echo($message);
-        include("./include/formInscription.php");
-    }
-    else {
-        $connexion = mysqli_connect("localhost", "nfactoryblog", "nfactoryblog", "nfactoryblog");
-        if (!$connexion) {
-            die("Erreur MySQL " . mysqli_connect_errno() . " : " . mysqli_connect_error());
-        }
-        else {
-            $requete = "INSERT INTO t_users (ID_USER, USERNAME, USERFNAME,
-                        USERMAIL, USERPASSWORD, USERDATEINS, T_ROLES_ID_ROLE)
-                        VALUES (NULL, '$nom', '$prenom', '$mail', '$hash', NULL, 5);";
-
-            if(mysqli_query($connexion, $requete))
-                echo("<p>Votre inscription a bien été effectuée. Vous allez prochainement recevoir un mail de confirmation.</p>");
-            else
-                echo("<p>Erreur sur la base de données, veuillez réessayer.</p>");
-
-            mysqli_close($connexion);
-        }
-    }
+}else{
+    include ("./include/formInscription.php");
 }
-else {
-    include("./include/formInscription.php");
-}
+  ?>
