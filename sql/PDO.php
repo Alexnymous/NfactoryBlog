18a18fb6c75b4995504ee08ffbb6ae8e5f426e1e
Alexnymous <32875309+Alexnymous@users.noreply.github.com>
2018-01-22 12:55:51 +0100

    création PDO

--- sql/PDO.php
+++ sql/PDO.php
@@ -1,20 +1,55 @@
 <?php
-$dsn = "mysqli::dbname = nfactoryblog;
-        host = localhost;
-        charset = utf-8";
+$dsn = "mysql:dbname=nfactoryblog;
+        host=localhost;
+        charset=utf8";
 
 $username = "root";
 $password = "";
 
 //$db = new PDO ($dsn, $username, $password);
 
-try (
+try {
 $db = new PDO ($dsn, $username, $password);
-)
+}
 
-catch (PDOException:: $e){
+catch (PDOException $e){
     echo ($e -> getmessage());
 }
 
+$sql = "SELECT * FROM t_articles";
+
+$resultat = $db -> query($sql);
+
+while (($article = $resultat -> fetch())){
+    echo $article ["ARTCONTENU"];
+}
+
+// requete insertion
+
+$sql = "INSERT INTO t_articles";
+$nbrLignes = $db -> exec($sql);
+echo $nbrLignes;
 
 unset($db);
+
+// Formatage par défaut
+$db = new PDO ($dsn, $username, $password);
+$db -> setAttribute(
+    PDO::ATTR_DEFAULT_FETCH_MODE,
+    PDO::FETCH_ASSOC
+);
+
+//Formatage pour un jeu de résultat
+$resultat = $db -> query("SELECT");
+$resultat -> setFetchMode(PDO::FETCH_ASSOC);
+
+while(($resultat = $article -> fetch())){
+    var_dump($resultat);
+}
+
+// A chaque appel
+
+$resultat = $db -> query ("SELECT");
+while (($article = $resultat -> fetch(PDO::FETCH_ASSOC))){
+    var_dump($article);
+}
