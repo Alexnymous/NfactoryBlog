60ddf1e9dcd08eda083febba873b92e4ec590fe9
Alexnymous <32875309+Alexnymous@users.noreply.github.com>
2018-01-22 12:55:26 +0100

    création PDO login

--- include/login.inc.php
+++ include/login.inc.php
@@ -1,53 +1,28 @@
 <h1>Login</h1>
 <?php
+$dsn = "mysql:dbname=nfactoryblog;
+        host=localhost;
+        charset=utf8";
 
-// on teste la déclaration de notre cookie
-if (isset($_COOKIE['pseudo'])) {
-	echo 'Bonjour '.$_COOKIE['pseudo'].' !';
-}
-else {
-	echo '';
+$username = "root";
+$password = "";
 
-}
+if(isset($_POST['mail']) && isset($_POST['password'])){
+    $query = $dsn->prepare ('SELECT USERPASSWORD FROM t_users where USERMAIL = :mail');
+    $query->bindParam (':mail', $_POST['mail']);
+    $query->execute();
+    $result = $query->fetch();
+    $hash = $result[0];
 
-if (isset($_POST['login'])) {
-    $tabErreur = array();
-    $mail = $_POST['mail'];
-    $password = $_POST['password'];
-    if ($mail == "")
-        array_push($tabErreur, "Veuillez saisir une adresse");
-    if ($password == "")
-        array_push($tabErreur, "Veuillez saisir un mot de passe");
-    if (count($tabErreur) > 0) {
-        $message = "<ul>";
-        for ($i = 0 ; $i < count($tabErreur) ; $i++) {
-            $message .= "<li>" . $tabErreur[$i] . "</li>";
-        }
-        $message .= "</ul>";
-        echo ($message);
-        include ("./include/formLogin.php");
-    }
-    else {
-        $connexion = mysqli_connect("localhost", "nfactoryblog", "nfactoryblog", "nfactoryblog");
-        if (!$connexion) {
-            die("Erreur MySQL " . mysqli_connect_errno() . " : " . mysqli_connect_error());
-        }
-        else {
-            $password = sha1($password);
-            $requete = "SELECT * FROM t_users WHERE USERMAIL='$mail' AND USERPASSWORD='$password'";
-            if($result = mysqli_query($connexion, $requete)) {
-                if (mysqli_num_rows($result) > 0) {
-                    $_SESSION['login'] = 1;
-                    echo ("<script>redirection(\"index.php?page=accueil\");</script>
-                    <a href=\"index.php?page=accueil\">Vous êtes authentifié, viendez à la page d'accueil</a>");
-                }
-                else
-                    $_SESSION['login'] = 0;
-            }
-        }
-        mysqli_close($connexion);
+    $correctPassword = password_verify($_POST['password'], $hash);
+
+    if($correctPassword){
+        echo "Bienvenu " .$_POST['USERMAIL'];
+    }else{
+        echo "Login ou Password incorrect";
     }
 }
-else {
+else{
     include ("./include/formLogin.php");
 }
+
